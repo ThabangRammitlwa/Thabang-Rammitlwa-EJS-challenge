c3f9d33a1772446f0077c534c4d5c8294d9660dd
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-28 10:26:31 +0200

    chapter3/functions

---
+++ Challenge2.js
@@ -0,0 +1,17 @@
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == ch) {
+        counted += 1;
+      }
+    }
+    return counted;
+  }
+  
+  function countBs(string) {
+    return countChar(string, "B");
+  }
+  
+  console.log(countBs("BBC"));
+  // → 2
+  console.log(countChar("kakkerlak", "k"));
